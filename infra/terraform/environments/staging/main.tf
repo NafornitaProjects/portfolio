83d614250106c1ac95c62562aabf10fa3c56a3a2
Edward Nafornita <contact@edwardnafornita.com>
2025-07-01 23:05:51 -0400

    Updated infra

--- infra/terraform/environments/staging/main.tf
+++ infra/terraform/environments/staging/main.tf
@@ -1,50 +1,38 @@
-provider "kubernetes" {
-  insecure = true
+resource "kubernetes_namespace_v1" "ns" {
+  metadata { name = var.namespace }
 }
 
-resource "kubernetes_manifest" "ns" {
-  manifest = {
-    apiVersion = "v1"
-    kind       = "Namespace"
-    metadata = {
-      name = var.namespace
+resource "kubernetes_ingress_v1" "ingress" {
+  metadata {
+    name        = "${var.app_name}-ingress"
+    namespace   = var.namespace
+    annotations = {
+      "cert-manager.io/cluster-issuer"                   = "cloudflare-issuer"
+      "traefik.ingress.kubernetes.io/router.entrypoints" = "websecure"
     }
   }
-}
-
-resource "kubernetes_manifest" "ingress" {
-  manifest = {
-    apiVersion = "networking.k8s.io/v1"
-    kind       = "Ingress"
-    metadata = {
-      name      = "${var.app_name}-ingress"
-      namespace = var.namespace
-      annotations = {
-        "cert-manager.io/cluster-issuer"                   = "cloudflare-issuer"
-        "traefik.ingress.kubernetes.io/router.entrypoints" = "websecure"
-      }
+  spec {
+    ingress_class_name = "traefik"
+    tls {
+      hosts      = [var.domain]
+      secret_name = "${var.app_name}-tls"
     }
-    spec = {
-      ingressClassName = "traefik"
-      tls = [{
-        hosts      = [var.domain]
-        secretName = "${var.app_name}-tls"
-      }]
-      rules = [{
-        host = var.domain
-        http = {
-          paths = [{
-            path     = "/"
-            pathType = "Prefix"
-            backend = {
-              service = {
-                name = "${var.app_name}-svc"
-                port = { number = 80 }
+    rule {
+      host = var.domain
+      http {
+        path {
+          path     = "/"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = "${var.app_name}-svc"
+              port {
+                number = 80
               }
             }
-          }]
+          }
         }
-      }]
+      }
     }
   }
 }
